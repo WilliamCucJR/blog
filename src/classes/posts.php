632b6cfaae6ad37bca5b7337a4968789a6d3e077
WilliamCucJR <wcuc96dev@gmail.com>
2024-10-03 18:38:58 -0600

    Integración de nuevos inserts en sql.init

--- src/classes/posts.php
+++ src/classes/posts.php
@@ -1,6 +1,7 @@
 <?php
 
-class posts{
+class posts
+{
     private $conn;
 
     public function __construct($conn)
@@ -8,7 +9,8 @@ class posts{
         $this->conn = $conn;
     }
 
-    public function getPostsByUser($intUserId){
+    public function getPostsByUser($intUserId)
+    {
         $sql = "SELECT a.*, b.*, DATE_FORMAT(a.creado_en, '%d-%m-%Y') as creado_en 
                 FROM publicaciones a
                 INNER JOIN usuarios b ON b.id = a.autor_id 
@@ -19,7 +21,8 @@ class posts{
         $result = $stmt->get_result();
 
         $posts = array();
-        while($row = $result->fetch_assoc()){
+        while ($row = $result->fetch_assoc()) {
+            $row['contenido'] = html_entity_decode($row['contenido']);
             $posts[] = $row;
         }
         return $posts;
